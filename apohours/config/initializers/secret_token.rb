f93e9572611b996b74746af0f8e2cd9ef0611bb4
Ann Chen <annchen@andrew.cmu.edu>
2016-01-03 00:58:41 -0500

    base rails app

---
+++ apohours/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Apohours::Application.config.secret_key_base = 'cc71236fee53fd302297afc0250e8bcd6cca185e9bd81f6e3936e5914bcff4f835f9a85879354f4669cfaeac7e8a8d592184b628471d49c0c2f701e18cc16e38'
---
+++ apohours/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Apohours::Application.config.session_store :cookie_store, key: '_apohours_session'
